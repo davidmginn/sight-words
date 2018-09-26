6ed20cea744e18004623c0d837c1b04dbf9f0a9d
Dave Ginn <davidmginn@daves-mbp.lan>
2018-09-25 21:59:07 -0400

    :/

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import './sight-words'
-import SightWords from './sight-words';
+import React, { Component, Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./sight-words";
+import SightWords from "./sight-words";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Ethan's Sight Words!</h1>
-        </header>
-        <div className="App-intro">
+      <Fragment>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to Ethan's Sight Words!</h1>
+          </header>
+        </div>
+        <div>
           <SightWords />
         </div>
-      </div>
+      </Fragment>
     );
   }
 }
--- src/sight-words/index.js
+++ src/sight-words/index.js
@@ -1,61 +1,102 @@
 import React, { Component, Fragment } from "react";
-import _ from 'lodash';
+import _ from "lodash";
+import { ListGroup, ListGroupItem, Button, ButtonGroup } from "react-bootstrap";
 
 const numbers = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten"
-]
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten"
+];
 
 const colors = [
-    "red",
-    "pink",
-    "purple",
-    "blue",
-    "brown",
-    "black",
-    "orange",
-    "green",
-    "white",
-    "yellow"
-]
+  "red",
+  "pink",
+  "purple",
+  "blue",
+  "brown",
+  "black",
+  "orange",
+  "green",
+  "white",
+  "yellow"
+];
 
-class SightWords extends Component{
+class SightWords extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: colors,
+      numbers: numbers
+    };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {
-            colors: colors,
-            numbers: numbers
-        };
-    }
+  componentDidMount = () => {
+    let words = _.flatten([this.state.colors, this.state.numbers]);
+    this.setState({
+      words: _.shuffle(words),
+      incorrect: []
+    });
+  };
 
-    componentDidMount = () => {
-        let words = _.flatten([this.state.colors, this.state.numbers]);
-        this.setState({
-            words: _.shuffle(words)
-        })
-    }
+  correct = word => {
+    let words = _.remove(this.state.words, w => {
+      return w !== word;
+    });
 
-    render(){
-        return(
-            <Fragment>
-                {_.map(this.state.words,(word, index) => (
-                    <div style={{
-                        fontSize:'1.75em'
-                    }
-                    } key={index}>{word}</div>
-                ))}
-            </Fragment>
-        )
-    }
+    this.setState({
+      words: words.length > 0 ? words : this.state.incorrect,
+      incorrect: words.length > 0 ? this.state.incorrect : []
+    });
+  };
+
+  incorrect = word => {
+    let words = _.remove(this.state.words, w => {
+      return w !== word;
+    });
+
+    let incorrect = this.state.incorrect;
+    incorrect.push(word);
+
+    this.setState({
+      words: words.length > 0 ? words : incorrect,
+      incorrect: words.length > 0 ? incorrect : []
+    });
+  };
+
+  render() {
+    console.log(this.state.words ? this.state.words.length : "");
+    return (
+      <Fragment>
+        <ListGroup>
+          {_.map(this.state.words, (word, index) => (
+            <ListGroupItem
+              style={{
+                fontSize: "1.75em"
+              }}
+              key={index}
+            >
+              {word}
+              <ButtonGroup className="pull-right">
+                <Button bsStyle="success" onClick={() => this.correct(word)}>
+                  Correct
+                </Button>
+                <Button bsStyle="danger" onClick={() => this.incorrect(word)}>
+                  Incorrect
+                </Button>
+              </ButtonGroup>
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </Fragment>
+    );
+  }
 }
 
 export default SightWords;
